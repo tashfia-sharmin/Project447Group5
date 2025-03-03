4320c94eff0510235649d7664785e5d35235afe2
tashfia <tashfia@cs.washington.edu>
2025-03-03 14:04:25 -0800

    updated for submit

--- src/myprogram.py
+++ src/myprogram.py
@@ -13,10 +13,11 @@ class MyModel:
     """
     This is a starter model to get you started. Feel free to modify this file.
     """
-    @classmethod
-    def __init__(self, n):
+    def __init__(self, n, alpha=1):
         self.n = n  # Size of n-grams
+        self.alpha = alpha  # Laplace smoothing factor
         self.model = defaultdict(Counter)
+        self.vocab = set()  # Track unique characters
 
     @classmethod
     def load_training_data(cls, file_path):
@@ -34,111 +35,158 @@ class MyModel:
 
     @classmethod
     def load_test_data(cls, file_path):
-        # your code here
+        # going out one file for predict.sh
+        if not os.path.exists(file_path):
+            work_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'example'))
+            file_path = os.path.join(work_dir, 'input.txt')
+
         with open(file_path, "r", encoding="utf-8") as file:
-            reader = csv.reader(file)
-            next(reader)  # Skip header
             data = []
-            for row in reader:
-                _, unicode_seq, frequency, language_id = row  # Ignore the Word column
-                unicode_seq = eval(unicode_seq)
-                frequency = int(frequency)
-                language_id = int(language_id)
-                data.append((unicode_seq, frequency, language_id))
+            for line in file:
+                unicode_seq = tuple(ord(c) for c in line.strip())  # Convert characters to Unicode
+                data.append(unicode_seq)
             return data
 
     @classmethod
     def write_pred(cls, preds, fname):
         with open(fname, 'wt') as f:
             for p in preds:
+                # print(p)
                 f.write('{}\n'.format(p))
 
     def run_train(self, data):
-        for unicode_seq, frequency, language_id in data:
+        # Build raw frequency counts
+        for unicode_seq, frequency, _ in data:
             for i in range(len(unicode_seq) - self.n + 1):
-                prefix = unicode_seq[i:i + self.n - 1]
-                next_char_unicode = unicode_seq[i + self.n - 1]
-                self.model[prefix][next_char_unicode] += frequency
+                prefix = tuple(unicode_seq[i:i + self.n - 1])
+                next_char = unicode_seq[i + self.n - 1]
+                self.model[prefix][next_char] += frequency
+                self.vocab.add(next_char)
+
+        vocab_size = len(self.vocab)
+
+        # Convert counts to probabilities with Laplace smoothing
+        for prefix, counts in self.model.items():
+            total = sum(counts.values()) + self.alpha * vocab_size
+            for char in self.vocab:
+                counts[char] = (counts.get(char, 0) + self.alpha) / total
 
     def run_pred(self, sequence_unicode, top_k=3):
-        # your code here
-        sequence_unicode = sequence_unicode[-(self.n - 1):]
-        if sequence_unicode in self.model:
-            predictions = self.model[sequence_unicode].most_common(top_k)
-            return [char for char, _ in predictions]
-        return []
+        sequence_unicode = tuple(sequence_unicode[-(self.n - 1):])  # Ensure correct prefix length
+
+        # Backoff approach: Try shorter prefixes if necessary
+        while sequence_unicode:
+            if sequence_unicode in self.model:
+                predictions = sorted(
+                    self.model[sequence_unicode].items(), key=lambda x: x[1], reverse=True
+                )
+                return [char for char, _ in predictions[:top_k]]
+            sequence_unicode = sequence_unicode[1:]  # Shorten prefix (backoff)
+
+        # If no match found, return most common characters in training data
+        most_common_chars = Counter()
+        for counts in self.model.values():
+            most_common_chars.update(counts)
+
+        return [char for char, _ in most_common_chars.most_common(top_k)]
 
     def save(self, work_dir):
-        # your code here
-        # this particular model has nothing to save, but for demonstration purposes we will save a blank file
-        os.makedirs(work_dir, exist_ok=True)
-        with open(os.path.join(work_dir, 'model.checkpoint'), 'wb') as f:
-             pickle.dump(self, f)
-
+        with open(os.path.join(work_dir, 'model.pkl'), 'wb') as f:
+            pickle.dump((self.n, self.alpha, self.model, self.vocab), f)
 
     @classmethod
     def load(cls, work_dir):
-        # your code here
-        with open(os.path.join(work_dir, 'model.checkpoint'), "rb") as f:
-            loaded_model = pickle.load(f)
+        work_dir = os.path.abspath('work')
+        file_path = os.path.join(work_dir, 'model.pkl')
+
+        # going out one file for predict.sh
+        if not os.path.exists(file_path):
+            work_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'work'))
+            file_path = os.path.join(work_dir, 'model.pkl')
+
+        with open(file_path, 'rb') as f:
+            n, alpha, model, vocab = pickle.load(f)
+        loaded_model = cls(n, alpha)
+        loaded_model.model = model
+        loaded_model.vocab = vocab
         return loaded_model
-# Quick method to return the character from the unicode
+
+# Quick method to return the character from Unicode
 def unicode_to_char(unicode):
     return chr(unicode)
 
-# Small TEST - this works!
-model = MyModel(n=4)
-data = model.load_training_data("train_split_en.csv")  # Load data
-model.run_train(data)  # Train the model
-model.save('work')
+# Quick method to return the Unicode from character
+def convert_to_uni(word):
+    return tuple(ord(char) for char in word)
+
+# Small TEST
+# model = MyModel(n=4)
+# data = model.load_training_data("src/train_split_en.csv")  # Load data
+# model.run_train(data)  # Train the model
+
+# # using example input.txt
+# test_data = model.load_test_data("example/input.txt")
+
+# predictions = []
+# for sequence in test_data:
+#     pred = model.run_pred(sequence)  # Predict for the current sequence
+#     predictions.append(''.join([chr(c) for c in pred]))  # Convert Unicode to characters
+
+# # Write predictions to a file
+# model.write_pred(predictions, 'pred.txt')
+
+# OLD
 # Predict next Unicode characters for the sequence "hel"
-model1 = MyModel.load('work')
-predictions = model1.run_pred((104, 101, 108))
-predictions = [unicode_to_char(p) for p in predictions]
-print(predictions)
-model.write_pred(predictions, 'pred.txt')
+# predictions = model.run_pred((104, 101, 108))
+# predictions = [unicode_to_char(p) for p in predictions]
+# print(predictions)
+# model.write_pred(predictions, 'output.txt')
 
 # Will work once we update to handle larger dataset -> think we need to update run_pred
 # i think we need to make sure this works tho
-# if __name__ == '__main__':
-#     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-#     parser.add_argument('mode', choices=('train', 'test'), help='what to run')
-#     parser.add_argument('--work_dir', help='where to save', default='work')
-#     parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
-#     parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
-#     args = parser.parse_args()
+if __name__ == '__main__':
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('mode', choices=('train', 'test'), help='what to run')
+    parser.add_argument('--work_dir', help='where to save', default='work')
+    parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
+    parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
+    args = parser.parse_args()
 
-#     random.seed(0)
+    random.seed(0)
 
-#     if args.mode == 'train':
-#         if not os.path.isdir(args.work_dir):
-#             print('Making working directory {}'.format(args.work_dir))
-#             os.makedirs(args.work_dir)
+    if args.mode == 'train':
+        if not os.path.isdir(args.work_dir):
+            print('Making working directory {}'.format(args.work_dir))
+            os.makedirs(args.work_dir)
 
-#         print('Instatiating model')
-#         model = MyModel(n=4)
+        print('Instatiating model')
+        model = MyModel(n=4)
 
-#         print('Loading training data')
-#         train_data = MyModel.load_training_data('train_split_en.csv')
+        print('Loading training data')
+        train_data = MyModel.load_training_data('src/train_split_en.csv')
 
-#         print('Training')
-#         model.run_train(train_data)
+        print('Training')
+        model.run_train(train_data)
 
-#         print('Saving model')
-#         model.save(args.work_dir)
+        print('Saving model')
+        model.save(args.work_dir)
 
-#     elif args.mode == 'test':
-#         print('Loading model')
-#         model = MyModel.load('work')
+    elif args.mode == 'test':
+        print('Loading model')
+        model = MyModel.load('work')
 
-#         print('Loading test data from {}'.format(args.test_data))
-#         test_data = MyModel.load_test_data(args.test_data)
+        print('Loading test data from {}'.format(args.test_data))
+        test_data = MyModel.load_test_data(args.test_data)
 
-#         print('Making predictions')
-#         pred = model.run_pred(test_data)
+        print('Making predictions')
+        predictions = []
+        for sequence in test_data:
+            pred = model.run_pred(sequence)  # Predict for the current sequence
+            predictions.append(''.join([chr(c) for c in pred]))
+        # pred = model.run_pred(test_data)
 
-#         print('Writing predictions to {}'.format(args.test_output))
-#         assert len(pred) == len(test_data), 'Expected {} predictions but got {}'.format(len(test_data), len(pred))
-#         model.write_pred(pred, args.test_output)
-#     else:
-#         raise NotImplementedError('Unknown mode {}'.format(args.mode))
+        print('Writing predictions to {}'.format(args.test_output))
+        # assert len(pred) == len(test_data), 'Expected {} predictions but got {}'.format(len(test_data), len(pred))
+        model.write_pred(predictions, args.test_output)
+    else:
+        raise NotImplementedError('Unknown mode {}'.format(args.mode))
